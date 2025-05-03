2344ea574b28ef41b2c1e7051877e2f819df5c6c
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-05-03 18:11:48 +0600

    инициализация проекта

---
+++ src/main/java/com/zaicev/task_tracker_email_sender/TaskTrackerEmailSenderApplication.java
@@ -0,0 +1,13 @@
+package com.zaicev.task_tracker_email_sender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerEmailSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackerEmailSenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zaicev/task_tracker_email_sender/TaskTrackerEmailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaicev.task_tracker_email_sender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerEmailSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
